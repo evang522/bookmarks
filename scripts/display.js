f8793c701cdbe39e875bd84c23b218b27782ce3f
Evan Garrett <evang522@gmail.com>
2018-01-25 15:43:20 -0500

    confirm that createItemSubmit->respondfrom API->renderItemString->pushtoDom works

--- scripts/display.js
+++ scripts/display.js
@@ -1,13 +1,37 @@
 'use strict';
 
+
+/*global api,localModel*/
+
 /* display*/
 const display = function () {
 
+  const listenForNewBookmarkSubmit = () => {
+    $('.add-bookmark-form').on('submit', (event) => {
+      event.preventDefault();
+      const title =  $('.add-bookmark-title').val();
+      $('.add-bookmark-title').val('');
+      const desc =  $('.add-bookmark-description').val();
+      $('.add-bookmark-description').val('');
+      const url =  $('.add-bookmark-url').val();
+      $('.add-bookmark-url').val('');
+      const rating =  $('.add-bookmark-rating').val();
+      $('.add-bookmark-rating').val('');
+      // Send to Server
+      api.createNewBookmarkOnServer(title,desc,rating,url, (response) => {
+        localModel.addSingleBookmarkToModel(response);
+        pushToDom(generateHTMLStringFromLocalBookmarks(localModel.bookmarks));
 
+      });
+
+    });
+
+  };
 
 
   const initiateApp = () => {
     //event listeners
+    listenForNewBookmarkSubmit();
   };
 
 
@@ -33,6 +57,7 @@ const display = function () {
     initiateApp,
     generateHTMLStringFromLocalBookmarks,
     pushToDom,
+    listenForNewBookmarkSubmit
   };
 
 }();
