ae54bfd1caae6814a9f9bd7daf5f3a51c9c7d684
Evan Garrett <evang522@gmail.com>
2018-01-25 15:01:46 -0500

    confirm function to pull all api items and single item into store works

--- scripts/localModel.js
+++ scripts/localModel.js
@@ -1,1 +1,36 @@
-console.log('state ran');
+'use strict';
+
+
+const localModel = function () {
+
+  const addSingleBookmarkToModel = function (bookmark) {
+    this.bookmarks.push(bookmark);    
+  };
+
+  const pullAllBookmarksIntoModel = function (bookmarksArrayFromServer) {
+    // This will never be used except for on initiating app.
+    bookmarksArrayFromServer.forEach((item) => {
+      this.bookmarks.push({
+        id:item.id,
+        title:item.title,
+        url:item.url,
+        desc:item.desc,
+        rating:item.rating,
+        isPreviewing:false
+      });
+    });
+  };
+
+  const bookmarks = [];
+
+
+  return {
+    bookmarks,
+    addSingleBookmarkToModel,
+    previewingBookmark:false,
+    filterRating:0,
+    pullAllBookmarksIntoModel
+    
+  };
+
+}();
